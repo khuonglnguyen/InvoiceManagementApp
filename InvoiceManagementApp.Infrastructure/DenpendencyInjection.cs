40051b0a931166f25b26c6092678dc4272576f99
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2022-10-25 10:19:56 +0700

    Part 1

---
+++ InvoiceManagementApp.Infrastructure/DenpendencyInjection.cs
@@ -0,0 +1,33 @@
+﻿using InvoiceManagementApp.Infrastructure.Data;
+using InvoiceManagementApp.Infrastructure.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InvoiceManagementApp.Infrastructure
+{
+    public static class DenpendencyInjection
+    {
+        public static IServiceCollection AddInfrastucture(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddIdentityServer()
+                .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+
+            services.AddAuthentication()
+                .AddIdentityServerJwt();
+            return services;
+        }
+    }
+}
