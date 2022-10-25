b24f50e725ef91721d7906cd4895bfb43e21d46f
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2022-10-25 14:49:53 +0700

    Part 4 AutoMapper

--- InvoiceManagementApp.Application/Invoices/Handlers/GetUserInvoicesQueryHandler.cs
+++ InvoiceManagementApp.Application/Invoices/Handlers/GetUserInvoicesQueryHandler.cs
@@ -1,4 +1,5 @@
-﻿using InvoiceManagementApp.Application.Common.Interfaces;
+﻿using AutoMapper;
+using InvoiceManagementApp.Application.Common.Interfaces;
 using InvoiceManagementApp.Application.Invoices.Queries;
 using InvoiceManagementApp.Application.Invoices.ViewModels;
 using MediatR;
@@ -15,40 +16,24 @@ namespace InvoiceManagementApp.Application.Invoices.Handlers
     public class GetUserInvoicesQueryHandler : IRequestHandler<GetUserInvoicesQuery, IList<InvoiceVm>>
     {
         private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
 
-        public GetUserInvoicesQueryHandler(IApplicationDbContext context)
+        public GetUserInvoicesQueryHandler(IApplicationDbContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
-
         public async Task<IList<InvoiceVm>> Handle(GetUserInvoicesQuery request, CancellationToken cancellationToken)
         {
+            var result = new List<InvoiceVm>();
             var invoices = await _context.Invoices.Include(i => i.InvoiceItems)
                 .Where(i => i.CreatedBy == request.User).ToListAsync();
-            var vm = invoices.Select(i => new InvoiceVm
+            if (invoices != null)
             {
-                AmountPaid = i.AmountPaid,
-                Date = i.Date,
-                Discount = i.Discount,
-                DiscountType = i.DiscountType,
-                DueDate = i.DueDate,
-                From = i.From,
-                Id = i.Id,
-                InvoiceNumber = i.InvoiceNumber,
-                Logo = i.Logo,
-                PaymentTerms = i.PaymentTerms,
-                Tax = i.Tax,
-                TaxType = i.TaxType,
-                To = i.To,
-                InvoiceItems = i.InvoiceItems.Select(i => new InvoiceItemVm
-                {
-                    Id = i.Id,
-                    Item = i.Item,
-                    Quantity = i.Quantity,
-                    Rate = i.Rate
-                }).ToList()
-            }).ToList();
-            return vm;
+                result = _mapper.Map<List<InvoiceVm>>(invoices);
+            }
+
+            return result;
         }
     }
 }
